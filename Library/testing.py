d5ced218f38e900523990ba088e031833991ea61
Joanna <geeitsjo@users.noreply.github.com>
2019-12-09 14:58:52 +1300

    affsdf
    testibng

---
+++ Library/testing.py
@@ -0,0 +1,12 @@
+logger = logging.getLogger()
+Access Key ID: 
+AKIAW6H6UZACACPKWXXS  
+AKIAW6T6UZACACMAZFXS  
+Secret Access Key: 
+7GiMlswo9JdlL/9DchBRntcMRNnh7R5ALLbd8dQf
+AWS Account ID: 208429635128
+
+
+Secret
+Key
+Access
